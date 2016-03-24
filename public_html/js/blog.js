20bace893379762e9eb45dbbee5463bddf926d47
Empoleo <iggydowell@gmail.com>
2016-03-24 11:01:36 -0700

    v9

--- public_html/js/blog.js
+++ public_html/js/blog.js
@@ -1,7 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(function () {
+    var APPLICATION_ID = "119CF616-6BCB-C750-FFE4-DF4F6FE81D00",
+    SECRET_KEY = "62F4611F-4E7F-AF77-FF61-C1A9DF55BC00",
+    VERSION = "v1";
+    
+    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+    
+    var user = new Backendless.user();
+    user.email = "matthewbrittain3d@gmail.com"
+    user.password = "password";
+    Backendless.userService.register(user);
+    
+});
